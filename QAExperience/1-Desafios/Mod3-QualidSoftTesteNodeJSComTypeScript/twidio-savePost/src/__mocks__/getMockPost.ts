84e0d3a7c711db00c7a2edeb113f4b3dfaf85616
KadarPegasus <sidmoreira.hp@gmail.com>
2023-06-03 15:03:38 -0300

    Deletando arquivos BootCamp QAexperience

--- QAExperience/1-Desafios/Mod3-QualidSoftTesteNodeJSComTypeScript/twidio-savePost/src/__mocks__/getMockPost.ts
+++
@@ -1,8 +0,0 @@
-import { v4 as uuid } from 'uuid'
-import { Post } from '../entities/Post'
-
-export const getMockPost = (): Post => ({
-  post_id: uuid(),
-  author: 'author@email.com',
-  content: 'Some content twit'
-})
