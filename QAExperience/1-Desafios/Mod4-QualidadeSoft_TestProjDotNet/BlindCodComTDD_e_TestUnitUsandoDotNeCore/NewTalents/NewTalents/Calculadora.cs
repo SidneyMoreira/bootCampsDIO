7bb1b69bf4a1eb3014492f32989f90ba9f1db4a3
KadarPegasus <sidmoreira.hp@gmail.com>
2023-06-04 01:00:34 -0300

    Acrescentando Desafio Projetos MOD4 BootCamp QAexperience

---
+++ QAExperience/1-Desafios/Mod4-QualidadeSoft_TestProjDotNet/BlindCodComTDD_e_TestUnitUsandoDotNeCore/NewTalents/NewTalents/Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewTalents
+{
+    public class Calculadora
+
+    {
+        
+        private List<string> listaHistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+           listaHistorico = new List<string>();
+            this.data = data;
+        }
+        public int somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+            listaHistorico.Insert(0, "Resultado Soma: " + res + " - " + data);
+            return res;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listaHistorico.Insert(0, "Resultado Subtracao: " + res + " - " + data);
+            return res;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listaHistorico.Insert(0, "Resultado Multiplicacao: " + res + " - " + data);
+            return res;
+        }
+
+        public int divir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            listaHistorico.Insert(0, "Resultado Divisao: " + res + " - " + data);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            
+            listaHistorico.RemoveRange(3, listaHistorico.Count-3 );
+            return listaHistorico;
+        }
+    }
+}
---
+++ QAExperience/1-Desafios/Mod4-QualidadeSoft_TestProjDotNet/BlindCodComTDD_e_TestUnitUsandoDotNeCore/NewTalents/TestNewTalents/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using NewTalents;
+using Xunit;
+
+namespace TestNewTalents
+{
+    public class UnitTest1
+    {
+        public Calculadora construirCalculadora()
+        {
+            string data = "11/12/1975";
+            Calculadora calc = new Calculadora(data);
+            return calc;
+        }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(4, 5, 9)]
+        public void TestSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirCalculadora();
+
+            int resultadoCalculadora = calc.somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(21, 2, 19)]
+        [InlineData(10, 3, 7)]
+        public void TestSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirCalculadora();
+
+            int resultadoCalculadora = calc.subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(21, 2, 42)]
+        [InlineData(10, 3, 30)]
+        public void TestMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirCalculadora();
+
+            int resultadoCalculadora = calc.multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(21, 7, 3)]
+        [InlineData(10, 2, 5)]
+        public void TestDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirCalculadora();
+
+            int resultadoCalculadora = calc.divir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = construirCalculadora();
+
+            Assert.Throws<DivideByZeroException>(
+                () => calc.divir(3, 0)
+            );
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = construirCalculadora();
+
+            calc.somar(4, 3);
+            calc.subtrair(15, 8);
+            calc.multiplicar(4, 8);
+            calc.divir(30, 7);
+
+            var lista = calc.historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
